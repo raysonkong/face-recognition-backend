8bfa78aeb94afdd389eb2480ba300fee99cdbf75
Rayson Kong <kong.rayson@gmail.com>
2020-06-01 18:20:05 +0800

    update server

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 const express = require('express');
-
 const app = express();
-
 const port = 3001;
 
 app.listen(port, () => {
-    console.log(`App is listening on port ${port}`);
+    console.log(`app is running on port: ${port}.`)
 })
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+
+const database = [
+  { 
+    id: '123',
+    name: "Amy",
+    email: "amy@gmail.com",
+    password: "123",
+    entries: 0,
+    joined: new Date()
+  },
+  
+  { 
+    id: '123',
+    name: "Amy",
+    email: "amy@gmail.com",
+    password: "123",
+    entries: 0,
+    joined: new Date()
+  },
+
+]
+
+
 app.get('/', (req, res) => {
-    res.json("Hello world server!")
+    res.json('Hello World!')
+})
+
+app.post('/signin', (req, res) => {
+    res.json("signing")
 })
